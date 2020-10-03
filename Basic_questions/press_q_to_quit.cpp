ede9a54749b2e05646b66b9d73213f56935010b8
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-03 23:25:01 +0530

    Added press q to quit file

---
+++ Basic_questions/press_q_to_quit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char t = 'q';
+    char n;
+    while (n != t)
+    {
+        cout << "\nEnter q to STOP !!!\n";
+
+        cout << "\nEnter a Character = ";
+
+        cin >> n;
+    }
+
+    return 0;
+}
