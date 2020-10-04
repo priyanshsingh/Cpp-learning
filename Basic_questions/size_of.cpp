2a350f37c2c854b525f5e103801a69567d035bd3
Priyansh Singh <64950658+priyanshsingh@users.noreply.github.com>
2020-10-04 19:45:11 +0530

    Add files via upload
    size_of.cpp added

---
+++ Basic_questions/size_of.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n\nThe size of int is = " << sizeof(int) << " bytes. \n\n";
+    cout << "The size of float is = " << sizeof(float) << " bytes. \n\n";
+    cout << "The size of double is = " << sizeof(double) << " bytes. \n\n";
+    cout << "The size of char is = " << sizeof(char) << " bytes. \n\n";
+    cout << "The size of wchar_t is = " << sizeof(wchar_t) << " bytes. \n\n";
+    cout << "The size of bool is = " << sizeof(bool) << " bytes.\n\n";
+
+    return 0;
+}
