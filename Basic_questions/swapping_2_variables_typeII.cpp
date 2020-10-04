aeccdec038ba1c064ddd95742e3916222afafa6c
Priyansh Singh <64950658+priyanshsingh@users.noreply.github.com>
2020-10-04 19:51:02 +0530

    Add files via upload
    swapping_2_variables_typeII.cpp added

---
+++ Basic_questions/swapping_2_variables_typeII.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter Number 1 = ";
+    cin >> a;
+    cout << endl;
+
+    cout << "Enter Number 2 = ";
+    cin >> b;
+    cout << endl;
+
+    a = a + b; // a=1 , b=2 , a+b=3 , for b=1 ==> 3-2 i.e. a-b
+
+    b = a - b;
+
+    a = a - b;
+
+    cout << "Here, the numbers are swapped \n"
+         << endl
+         << "The first number becomes " << a << endl
+         << "And second number becomes " << b << endl;
+
+    return 0;
+}
