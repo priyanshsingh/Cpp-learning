2ead99e27cf305f95c07a8a0f86641e7c9e1403f
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-09 23:42:54 +0530

    Modified practical 4

--- C_Practicals/Practical 1/Practical_1.4.c
+++ C_Practicals/Practical 1/Practical_1.4.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main()
+
 {
-    
+
+    int m, n, result;
+
+    printf("Enter the postion of Sonu from top: ");
+
+    scanf("%d", &m);
+
+    printf("\n");
+
+    printf("Enter the postion of Sonu from bottom: ");
+
+    scanf(" %d", &n);
+
+    result = m + n - 1;
+
+    printf("How many students are there in the class : %d ", result);
+
     return 0;
 }
