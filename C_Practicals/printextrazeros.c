359b4f2831988bc29bfbb22406e2b76f0b4136f0
Priyansh Singh <64950658+priyanshsingh@users.noreply.github.com>
2020-10-10 23:06:29 +0530

    Add files via upload
    printextrazeros file

---
+++ C_Practicals/printextrazeros.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int x;
+    printf("Enter a number in integer format = ");
+    scanf("%d", &x);
+    printf("\nThe entered number is = %d", x);
+    printf("\nThe entered number is = %02d", x);
+    printf("\nThe entered number is = %03d", x);
+    printf("\nThe entered number is = %04d", x);
+    printf("\nThe entered number is = %05d", x); 
+    printf("\nThe entered number is = %06d", x);
+    printf("\nThe entered number is = %07d", x);
+    printf("\nThe entered number is = %08d", x);
+    printf("\nThe entered number is = %09d", x);
+    
+        return 0;
+}
