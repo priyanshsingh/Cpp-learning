5e3f2e7e72f0a9dff878576fd7e2b643435d4a93
Priyansh Singh <64950658+priyanshsingh@users.noreply.github.com>
2020-10-10 23:03:37 +0530

    Add files via upload
    Printwithspaces file added

---
+++ C_Practicals/printwithspaces.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int x;
+    printf("Enter a number in integer format = ");
+    scanf("%d", &x);
+    printf("\nThe entered number is = %d", x);
+    printf("\nThe entered number is = %2d", x);
+    printf("\nThe entered number is = %3d", x);
+    printf("\nThe entered number is = %4d", x);
+    printf("\nThe entered number is = %5d", x); 
+    printf("\nThe entered number is = %6d", x);
+    printf("\nThe entered number is = %7d", x);
+    printf("\nThe entered number is = %8d", x);
+    printf("\nThe entered number is = %9d", x);
+    
+        return 0;
+}
