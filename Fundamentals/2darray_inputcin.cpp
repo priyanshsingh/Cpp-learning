00c110ab168b119105c3a372647779d31975e401
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-12 23:41:28 +0530

    added 2darray input cin file

---
+++ Fundamentals/2darray_inputcin.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cout << "Enter the number of rows = ";
+    cin >> m;
+    cout << "Enter the number of coloumns = ";
+    cin >> n;
+
+    int myarray1[m][n];
+
+    cout << "Enter Values for a " << m << " x " << n << " matrix = " << endl;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+
+            cin >> myarray1[i][j];
+        }
+    }
+
+    cout << "Values for the 2x2 matrix you entered are = " << endl;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+
+            cout << myarray1[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
