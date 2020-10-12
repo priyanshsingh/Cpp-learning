9475ac1bfbe5827683b5db1de494df55c6948b37
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-12 23:47:46 +0530

    modified a file

--- Fundamentals/2darray_inputcin.cpp
+++ Fundamentals/2darray_inputcin.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-    int m, n;
-    cout << "Enter the number of rows = ";
+    int m, n, p, q;
+    cout << "1. Enter the number of rows = ";
     cin >> m;
-    cout << "Enter the number of coloumns = ";
+    cout << "1. Enter the number of coloumns = ";
     cin >> n;
 
     int myarray1[m][n];
@@ -17,22 +17,31 @@ int main()
     {
         for (int j = 0; j < n; j++)
         {
-
             cin >> myarray1[i][j];
         }
     }
 
-    cout << "Values for the 2x2 matrix you entered are = " << endl;
+    cout << endl;
+
+    cout << "2. Enter the number of rows = ";
+    cin >> p;
+    cout << "2. Enter the number of coloumns = ";
+    cin >> q;
+
+    int myarray2[p][q];
+
+    cout << "Enter Values for a " << p << " x " << q << " matrix = " << endl;
 
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-
-            cout << myarray1[i][j] << " ";
+            cin >> myarray2[i][j];
         }
-        cout << endl;
     }
 
+    cout << endl;
+
+    
     return 0;
 }
