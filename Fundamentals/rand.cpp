908c6607d00f87534639cfc87aec84a650eccf0b
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-13 00:41:49 +0530

    added rand.cpp file

---
+++ Fundamentals/rand.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    cout << "Here are somr random numbers = \n";
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << 1 + (rand() % 6) << endl;
+    }
+
+    return 0;
+}
