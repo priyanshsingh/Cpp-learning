836ff0d65efd9f750886ed6c2ca44239afb1db01
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-13 13:50:00 +0530

    Added ascii print

---
+++ Fundamentals/ascii_print.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // int a = 0;
+    // char x = a;
+    // cout << x;
+    char x[255];
+
+    for (int i = 0; i <= 255; i++)
+    {
+        x[i] = i;
+    }
+
+    for (int j = 0; j <= 255; j++)
+    {
+        cout << j << ". " << x[j] << "\n";
+    }
+
+    return 0;
+}
