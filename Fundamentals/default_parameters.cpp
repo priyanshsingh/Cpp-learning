5c910eed7ae8c7d284394664739cfd556eea3131
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-14 23:22:36 +0530

    modified default parameters file

--- Fundamentals/default_parameters.cpp
+++ Fundamentals/default_parameters.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int add(int w, int x, int y = 0, int z = 5)
+{
+    return (w + x + y + z);
+}
 int main()
 {
+    cout << "The addition of 1, 2, 3, 4 is = " << add(1, 2, 3, 4) << endl;
+
+    cout << "The addition of 1, 2, 0, 5 is = " << add(1, 2) << endl;
 
     return 0;
 }
