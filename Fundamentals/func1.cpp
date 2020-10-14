a94256d8a445291f8f34cfa82a7fe812dcce0667
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-14 22:42:45 +0530

    added first function file

---
+++ Fundamentals/func1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printmessage()
+{
+    cout << "Hello World, this is the first function program which I made in C++!!! \n";
+}
+
+int main()
+{
+    printmessage();
+    return 0;
+}
