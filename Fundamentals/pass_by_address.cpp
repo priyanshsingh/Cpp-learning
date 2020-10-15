459b2a3cbbc72cef1e34f0f85ddf41dce134e5a1
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-15 23:24:38 +0530

    Added pass by address function calling

---
+++ Fundamentals/pass_by_address.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void passbyvalue(int x, int y)
+{
+    int z = x;
+    x = y;
+    y = z;
+}
+
+void passbyreference(int &x, int &y)
+{
+    int z = x;
+    x = y;
+    y = z;
+}
+
+void passbyaddress(int *x, int *y)
+{
+    int z = *x;
+    *x = *y;
+    *y = z;
+}
+
+int main()
+{
+    int a = 5, b = 6;
+    cout << "Before swapping the value of a = " << a << " and b = " << b << "\n";
+
+    passbyaddress(&a, &b);
+
+    cout << "Before swapping the value of a = " << a << " and b = " << b << "\n";
+
+    return 0;
+}
