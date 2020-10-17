59b5195db448d10848e183d5597ee52eb84c824b
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-17 19:06:45 +0530

    added practical 2.2

---
+++ C_Practicals/Practical 2/practical_2.2.c
@@ -0,0 +1,30 @@
+/*Inside the CPU, mathematical operations like addition, subtraction, multiplication and division are done in bit-level.
+To perform bit-level operations in C programming, bitwise operators are used.
+Apply the knowledge you gained while learning bitwise operators.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("\nEnter values of a and b = ");
+    scanf("%d %d", &a, &b);
+
+    printf("\n****************************\n", a, b);
+
+    printf("1. a = %d, b = %d\n", a, b);
+
+    printf("2. a & b = %d\n", a & b);
+
+    printf("3. a | b = %d\n", a | b);
+
+    printf("4. a ^ b = %d\n", a ^ b);
+
+    printf("5. ~ a  = %d\n", a = ~a);
+
+    printf("6. b << 1 = %d\n", b << 1);
+
+    printf("7. b >> 1 = %d\n", b >> 1);
+
+    return 0;
+}
