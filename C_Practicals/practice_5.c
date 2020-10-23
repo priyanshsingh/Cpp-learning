6d21a7952af20c1d28a7dcf917b0489c2fd1ec4b
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-23 12:35:38 +0530

    Added practice_5.c

---
+++ C_Practicals/practice_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter year = ");
+    scanf("%d", &x);
+
+    if (x % 400 == 0)
+    {
+        printf("%d is a leap year.", x);
+    }
+
+    else if (x % 100 == 0)
+    {
+        printf("%d is not a leap year.", x);
+    }
+
+    else if (x % 4 == 0)
+    {
+        printf("%d is a leap year.", x);
+    }
+
+    else
+    {
+        printf("%d is not a leap year.", x);
+    }
+
+    return 0;
+}
