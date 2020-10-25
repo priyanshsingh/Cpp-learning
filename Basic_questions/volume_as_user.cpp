91603e06f41d785d48fee12011e72adffc14452b
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-25 23:39:13 +0530

    added volume as per users choice

---
+++ Basic_questions/volume_as_user.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void vol(a)
+{
+    float volume = a * a * a;
+    cout << "Volume of cube " << i << " is = " << volume;
+}
+
+int main()
+{
+    int n;
+    cout << "How many times you want the volume to be found = ";
+    cin >> a1;
+
+    for (int i = 1, i <= n, i++)
+    {
+        vol(a);
+    }
+
+    cout << endl;
+    return 0;
+}
