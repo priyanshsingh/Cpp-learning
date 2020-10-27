d4318adecdaff1218befec45a88783103d6e8124
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-27 22:34:15 +0530

    modified number b/w 1 - 100

--- Basic_questions/number_1to100.cpp
+++ Basic_questions/number_1to100.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n, result;
     cout << "Enter any number between 1 and 100 (1 and 100 including) = ";
     cin >> n;
     cout << endl;
 
-    for (int i = 0; i == n; i++)
+    for (int i = 0; i <= n; i++)
     {
-        cout << "Enteren number was = " << i;
+        result = i;
     }
+    cout << "Entered number is = " << result;
 
     return 0;
 }
