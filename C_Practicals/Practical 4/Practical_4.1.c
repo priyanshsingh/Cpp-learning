ad110539fd5483a5bf88e166ededf3e9da9d4a21
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-29 22:52:18 +0530

    modified practical 4.1

--- C_Practicals/Practical 4/Practical_4.1.c
+++ C_Practicals/Practical 4/Practical_4.1.c
@@ -10,7 +10,14 @@ int main()
     {
         i--;
     }
-    printf("Largest 6 digit integer is %ld", i);
+    printf("\nLargest 6 digit integer is %ld", i);
+
+    long j = 1000000;
+    while (j % p != 0)
+    {
+        j++;
+    }
+    printf("\nSmallest 7 digit integer divisible by %d is %ld", p, j);
 
     return 0;
 }
