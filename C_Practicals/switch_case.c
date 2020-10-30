0f5a36c4eb490e1517bf0cfd1947a116484f0e92
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-30 10:10:42 +0530

    modified switch case

--- C_Practicals/switch_case.c
+++ C_Practicals/switch_case.c
@@ -9,32 +9,23 @@ int main()
     switch (x)
     {
     case 'a':
-        printf("A vowel");
-        break;
+
     case 'o':
-        printf("A vowel");
-        break;
+
     case 'i':
-        printf("A vowel");
-        break;
+
     case 'e':
-        printf("A vowel");
-        break;
+
     case 'u':
-        printf("A vowel");
-        break;
+
     case 'A':
-        printf("A vowel");
-        break;
+
     case 'E':
-        printf("A vowel");
-        break;
+
     case 'I':
-        printf("A vowel");
-        break;
+
     case 'O':
-        printf("A vowel");
-        break;
+
     case 'U':
         printf("A vowel");
         break;
