3a96e3bb08d08cb915dbdb2f74acbac8b6dc0adb
priyanshsingh <priyanshsingh1401@gmail.com>
2020-10-30 10:18:41 +0530

    added month switch

---
+++ C_Practicals/month_switch.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("Enter a month number = ");
+    scanf("%d", &x);
+
+    switch (x)
+    {
+    case 1:
+
+    case 3:
+
+    case 5:
+
+    case 7:
+
+    case 9:
+
+    case 10:
+
+    case 12:
+        printf("31 DAYS");
+        break;
+
+    case 4:
+
+    case 6:
+
+    case 8:
+
+    case 11:
+        printf("30 DAYS");
+        break;
+    default:
+        printf("28 DAYS");
+        break;
+    }
+}
