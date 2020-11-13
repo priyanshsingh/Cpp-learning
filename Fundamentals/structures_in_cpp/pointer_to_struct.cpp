52db693170dd0ed59007fc32d8e4dedcbc82c2b8
priyanshsingh <priyanshsingh1401@gmail.com>
2020-11-13 23:35:32 +0530

    modified to structure in cpp

--- Fundamentals/structures_in_cpp/pointer_to_struct.cpp
+++ Fundamentals/structures_in_cpp/pointer_to_struct.cpp
@@ -13,14 +13,25 @@ int main()
 {
     Person p1;
     cout << "Enter Details of person as Name, Age and Salary = ";
-    cin >> p1.name;
-    cin >> p1.age;
-    cin >> p1.salary;
+    // cin >> p1.name;
+    // cin >> p1.age;
+    // cin >> p1.salary;
+
+    // cout << "\nDetails of person as Name, Age and Salary are = ";
+    // cout << "Name is = " << p1.name << endl;
+    // cout << "Age is = " << p1.age << endl;
+    // cout << "Salary is = " << p1.salary << endl;
+
+    Person *ptr = &p1;
+
+    cin >> ptr->name;
+    cin >> ptr->age;
+    cin >> ptr->salary;
 
     cout << "\nDetails of person as Name, Age and Salary are = ";
-    cout << "Name is = " << p1.name << endl;
-    cout << "Age is = " << p1.age << endl;
-    cout << "Salary is = " << p1.salary << endl;
+    cout << "\nName is = " << ptr->name << endl;
+    cout << "Age is = " << ptr->age << endl;
+    cout << "Salary is = " << ptr->salary << endl;
 
     return 0;
 }
