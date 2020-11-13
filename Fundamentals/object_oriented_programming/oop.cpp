beab4ed51944804609a2c148d06567bae1ffa9eb
priyanshsingh <priyanshsingh1401@gmail.com>
2020-11-13 23:59:19 +0530

    added oop in fundamentals folder

--- Fundamentals/object_oriented_programming/oop.cpp
+++ Fundamentals/object_oriented_programming/oop.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Cars
+{
+private:
+    string company_name;
+    string model_name;
+    string fuel_type;
+    float mileage;
+    double price;
+
+public:
+    void set_data(string cname, string mname, string stype, float m, double p)
+    {
+    }
+};
 
 int main()
 {
