4f8a443e2925d8f21b06c6856491e39c5183c6a4
priyanshsingh <priyanshsingh1401@gmail.com>
2020-11-21 20:53:28 +0530

    modified constructor and destructors file

--- Fundamentals/object_oriented_programming/const_dest.cpp
+++ Fundamentals/object_oriented_programming/const_dest.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Cars
+{
+private:
+    string company_name;
+    string model_name;
+    string fuel_type;
+    float mileage;
+    double price;
+
+public:
+    Cars()
+    {
+        cout << "Default Constructor Called\n";
+    }
+
+    void set_data(string cname, string mname, string ftype, float m, double p)
+    {
+        company_name = cname;
+        model_name = mname;
+        fuel_type = ftype;
+        mileage = m;
+        price = p;
+    }
+
+    void display_data()
+    {
+        cout << "Car Properties are : " << endl
+             << endl;
+        cout << "Company Name = " << company_name << endl;
+        cout << "Model Name = " << model_name << endl;
+        cout << "Fuel Type = " << fuel_type << endl;
+        cout << "Mileage = " << mileage << endl;
+        cout << "Price = " << price << endl;
+    }
+};
 
 int main()
 {
+    Cars car1, car2;
+    car1.set_data("Toyota", "Altis", "Petrol", 22.00, 1500000);
+    car1.display_data();
 
-    reture 0;
+    return 0;
 }
