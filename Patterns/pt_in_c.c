ddc60b4643f1fe320eab7dc2207771364cef1513
priyanshsingh <priyanshsingh1401@gmail.com>
2020-11-25 11:40:36 +0530

    added ptinc

---
+++ Patterns/pt_in_c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, r;
+    char ch = 'A';
+    printf("Enter row size:");
+    scanf("%d", &r);
+    for (i = 1; i <= r; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c", ch + j - 1);
+        }
+        printf("\n");
+    }
+}
