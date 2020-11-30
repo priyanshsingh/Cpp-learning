db61cb4755e7562843885cb6602508fc6b0727c1
Priyansh Singh <64950658+priyanshsingh@users.noreply.github.com>
2020-11-30 22:43:53 +0530

    Add files via upload
    Practical 4.1 uploaded

--- C_Practicals/Practical 4/Practical_4.1.c
+++ C_Practicals/Practical 4/Practical_4.1.c
@@ -1,23 +1,60 @@
 #include <stdio.h>
 
 int main()
-{
-    int p;
-    printf("Enter number to check divisibility with = ");
-    scanf("%d", &p);
-    long i = 999999;
-    while (i % p != 0)
-    {
-        i--;
-    }
-    printf("\nLargest 6 digit integer is %ld", i);
 
-    long j = 1000000;
-    while (j % p != 0)
+{
+
+    int n, p, i;
+
+    printf("Enter total amount of things that are to be sold = ");
+    scanf("%d", &n);
+    printf("\n");
+
+    float item[n];
+
+    printf("Enter the cost of each product: \n");
+    for (int i = 0; i < n; i++)
     {
-        j++;
+        printf("Enter amount of item number %d = ", i + 1);
+        scanf("%f", &item[i - 1]);
     }
-    printf("\nSmallest 7 digit integer divisible by %d is %ld", p, j);
+
+    float m, x, profit = 0, loss = 0, total;
+
+    printf("\nEnter number of profiable items = ");
+    scanf("%d", &p);
+
+    printf("Enter the percentage of profit = ");
+    scanf("%f", &m);
+
+    printf("Items for loss are = %d", n - p);
+
+    printf("\nEnter the percentage of loss in remaining item : ");
+    scanf("%f", &x);
+
+    for (i = 0; i < p; i++)
+    {
+        profit += (m * item[i]) / 100;
+    }
+
+    for (i = p; i < n; i++)
+    {
+        loss += (x * item[i]) / 100;
+    }
+
+    total = profit - loss;
+
+    printf("\n------------------------------------------\n");
+
+    if (total > 0)
+        printf("Salesman gets Rs. %.2f profit", total);
+
+    else if (total < 0)
+        printf("Salesman gets Rs. %.2f loss", total * -1);
+
+    else
+
+        printf("Salesman got neither profit nor loss");
 
     return 0;
 }
