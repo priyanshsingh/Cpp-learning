bcca25e6b6fe6ebb7fbd7283b3f75349d66b7b93
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-09 19:08:27 +0530

    added 5.4.c

---
+++ C_Practicals/Practical 5/5.4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int n;
+
+    printf("Enter size of array : ");
+
+    scanf("%d", &n);
+
+    int a[n], x;
+
+    printf("Enter %d numbers for the array = ", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = n - 5; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            x = a[j];
+            a[j] = a[j - 1];
+            a[j - 1] = x;
+        }
+    }
+
+    printf("Updated arrangement = ");
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
