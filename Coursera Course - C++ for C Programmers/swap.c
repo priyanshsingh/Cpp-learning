0e1f3df18d343c856891f18887d86cd9c21bcde0
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-10 22:33:39 +0530

    Added swap.c file

--- Coursera Course - C++ for C Programmers/swap.c
+++ Coursera Course - C++ for C Programmers/swap.c
@@ -3,10 +3,11 @@
 int main()
 {
     int m = 5, n = 10;
-    double x = 5.6, y = 10.8;
     printf("Input: %d and %d", m, n);
     swap(&m, &n);
     printf("Output: %d and %d", m, n);
+
+    double x = 5.6, y = 10.8;
 
     return 0;
 }
