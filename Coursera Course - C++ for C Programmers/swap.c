16b714e3138f15d6dddfc14a460e9faac0f932f8
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-10 22:36:45 +0530

    modified swap.c file

--- Coursera Course - C++ for C Programmers/swap.c
+++ Coursera Course - C++ for C Programmers/swap.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void swap(int *i, int *j)
+{
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+void swap_double(int *i, int *j)
+{
+    double temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
 int main()
 {
     int m = 5, n = 10;
@@ -8,6 +22,8 @@ int main()
     printf("Output: %d and %d", m, n);
 
     double x = 5.6, y = 10.8;
-
+    printf("Double Input: %lf and %lf", x, y);
+    swap_double(&x, &y);
+    printf("Double Output: %lf and %lf", m, n);
     return 0;
 }
