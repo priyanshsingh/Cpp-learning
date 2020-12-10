3563ff1f367ee198fb877fd01a66399512c3a382
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-10 22:49:18 +0530

    added swap_overload.cpp file

---
+++ Coursera Course - C++ for C Programmers/swap_overload.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iostream>
+using namespace std;
+
+inline void swap(int &i, int &j)
+{
+    int temp = i;
+    i = j;
+    j = temp;
+}
+
+inline void swap(double &i, double &j)
+{
+    double temp = i;
+    i = j;
+    j = temp;
+}
+
+int main()
+{
+    int m = 5, n = 10;
+    cout << "\nInput: " << m << " & " << n;
+    swap(m, n);
+    cout << "\nOutput: " << m << " & " << n;
+
+    double x = 5.6, y = 10.8;
+    cout << "\n\nDouble Input: " << x << " & " << y;
+    swap(x, y);
+    cout << "\n\nDouble Output: " << x << " & " << y;
+
+    return 0;
+}
