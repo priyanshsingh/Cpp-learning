4059a4524945d3c2e8fac565b9cc47d568c0ef35
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-11 11:44:45 +0530

    added words.c.c

--- C_Practicals/class_practice/concatination.c
+++ C_Practicals/class_practice/concatination.c
@@ -1,39 +1,16 @@
 #include <stdio.h>
-
 #include <string.h>
 
 int main()
-
 {
-
-    char s[20], b[20];
-    int i, l = 0, j, flag = 0;
-    puts("Enter your string s:");
-    gets(s); //aabbaa
-    for (i = 0; s[i] != '\0'; i++)
+    char s[50];
+    printf("Enter the string = ");
+    gets(s);
+    int l = 0;
+    for (int i = 0; s[i] == ' '; i++)
     {
         l++;
     }
-
-    for (i = l - 1, j = 0; i >= 0; i--, j++)
-    {
-        b[j] = s[i];
-    }
-
-    b[j] = '\0';
-    puts("Reverse string is");
-    puts(b);
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] != b[i])
-        {
-            flag = 1;
-            printf("NOT PALINDROM");
-            break;
-        }
-    }
-    if (flag == 0)
-        printf("PALINDROM");
-
+    printf("Number of words in the string = %d", l + 2);
     return 0;
 }
