f8526ed7a6ea78f312556920acd29ac5d0077bff
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-11 13:33:39 +0530

    uplaoded python codes

---
+++ Python/Experiment 2/6_for.py
@@ -0,0 +1,10 @@
+# Loopy String
+# Demonstrates the for loop with a string
+
+word = input("Enter a word: ")
+
+print("\nHere's each letter in your word:")
+for let in word:
+    print(let)
+
+input("\n\nPress the enter key to exit.")
