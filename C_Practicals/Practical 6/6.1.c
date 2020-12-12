4afb4889b3b3c5ef9b69bf133331703ec3298e15
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-12 12:49:23 +0530

    added 6.1.c

--- C_Practicals/Practical 6/6.1.c
+++ C_Practicals/Practical 6/6.1.c
@@ -13,3 +13,42 @@
 
 // She typed a random string of length n in the password field but wasnt sure if it was strong.
 // Given the string she typed, can you find the minimum number of characters she must add to make her password strong?
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char password[15];
+    printf("Enter a password: \n");
+    gets(password);
+
+    for (int i = 0; i < 15; i++)
+    {
+        if (password[i] == ' ')
+        {
+            printf("Remove spaces from the password!");
+        }
+    }
+
+    int more_char;
+    if (strlen(password) < 6)
+    {
+        more_char = 6 - strlen(password);
+        printf("\nPassword is small, enter %d more characters!", more_char);
+    }
+    else if (strlen(password) == 6)
+    {
+        printf("\nPassword is just accurate in terms of length!", more_char);
+    }
+    else
+    {
+        printf("\nPassword length is strong!");
+    }
+
+    for (int i = 0; password[i] == ' '; i++)
+    {
+        printf("Remove spaces from the password!");
+    }
+    return 0;
+}
