42e58dee0005861ae22b4e15419a2acc5008623d
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-12 12:33:05 +0530

    added practical 6

---
+++ C_Practicals/Practical 6/6.1.c
@@ -0,0 +1,15 @@
+// Sonali joined a social networking site to stay in touch with her friends. The signup page required her to input a name and a password.
+// However, the password must be strong. The website considers a password to be strong if it satisfies the following criteria:
+
+// Its length is at least 6.
+
+// It contains at least one digit.
+
+// It contains at least one lowercase English character.
+
+// It contains at least one uppercase English character.
+
+// It contains at least one special character. The special characters are: !@#$%^&*()-+
+
+// She typed a random string of length n in the password field but wasnt sure if it was strong.
+// Given the string she typed, can you find the minimum number of characters she must add to make her password strong?
