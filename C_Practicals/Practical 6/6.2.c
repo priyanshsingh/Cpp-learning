3681b901f22e039a8b438f269c2f23e61b0725bd
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-13 23:27:32 +0530

    modified 6.2.c

--- C_Practicals/Practical 6/6.2.c
+++ C_Practicals/Practical 6/6.2.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    int len;
+    char string1[20];
+    printf("Enter a string: \n");
+    scanf("%[^\n]%*c", string1);
 
+    len = strlen(string1);
+
+    for (int i = 1; i <= len; i++)
+    {
+        for (int j = 0, k = 0; k != len; j++)
+        {
+            for (k = j; k < (j + i); k++)
+            {
+                printf("%c", string1[k]);
+            }
+            printf("\t");
+        }
+    }
 
     return 0;
 }
