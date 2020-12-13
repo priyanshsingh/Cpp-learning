203725d955acf7fc4748e4540599369cd083d24f
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-13 23:41:57 +0530

    modified 6.3.c

--- C_Practicals/Practical 6/6.3.c
+++ C_Practicals/Practical 6/6.3.c
@@ -3,25 +3,44 @@
 
 int main()
 {
-    int n;
+    int n, flag = 0;
     char name[30][20];
+    char character;
 
     printf("How many names do you need to be entered = ");
     scanf("%d", &n);
 
     printf("Enter names of your friends: ");
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <= n; i++)
     {
         gets(name[i]);
     }
 
     printf("Friends names are: ");
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i <= n; i++)
     {
         puts(name[i]);
     }
+
+    printf("Enter a character to search the name with: ");
+    scanf("%c", &character);
+
+    int i = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (name[i][0] == character)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag != 0)
+        printf("Name with first character %c is: %s", character, name[i]);
+    else
+        printf("Name not flag in array of given names");
 
     return 0;
 }
