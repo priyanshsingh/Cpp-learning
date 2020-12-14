b9eb7cdd6d8f5a684b1e763289dbfd17ab6f44f4
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-14 22:04:29 +0530

    modified sum of natural numbers.cpp

--- Basic_questions/functions/sum_natural_nos.cpp
+++ Basic_questions/functions/sum_natural_nos.cpp
@@ -11,12 +11,19 @@ int sum(int n)
     return answer;
 }
 
+int sum1(int n)
+{
+    int answer = (n * (n + 1)) / 2;
+
+    return answer;
+}
+
 int main()
 {
     int n;
     cin >> n;
 
-    cout << sum(n) << endl;
+    cout << sum1(n) << endl;
 
     return 0;
 }
