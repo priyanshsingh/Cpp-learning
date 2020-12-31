7d1e1b0c7dd81ec06899943752daed851c23dc3d
priyanshsingh <priyanshsingh1401@gmail.com>
2020-12-31 22:12:16 +0530

    added pointer1.c

--- C_Practicals/class_practice/Pointers/pointer1.c
+++ C_Practicals/class_practice/Pointers/pointer1.c
@@ -2,7 +2,15 @@
 
 int main()
 {
+    int x;
 
+    printf("Enter value of x = ");
+    scanf("%d", &x);
+
+    int *ptr;
+    ptr = &x;
+
+    printf("Address if integer x = %d is %d", x, ptr);
 
     return 0;
 }
