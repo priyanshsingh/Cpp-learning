4d0fa140500698875a017b20ba655864a71d4051
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-02 15:57:53 +0530

    modified min max element in array

--- C_Practicals/class_practice/Pointers/max_min_array_element.c
+++ C_Practicals/class_practice/Pointers/max_min_array_element.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-min_max(int arr[], int n, int *min, int *max)
+void min_max(int arr[], int n, int *min, int *max)
 {
     *min = *max = arr[0];
     int i;
-    for (i = 0; i < n, i++)
+    for (i = 0; i < n; i++)
     {
         if (arr[i] > *max)
             *max = arr[i];
@@ -41,5 +41,7 @@ int main()
 
     printf("\n*************************************************\n");
     printf("\nLargest element of the entered array is %d\nSmallest element of the entered array is %d", max, min);
+    printf("\n*************************************************\n");
+
     return 0;
 }
