5e7495487f240d39e198d78360390f3beb37d225
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-08 10:20:14 +0530

    added pointer array 3.c

--- C_Practicals/class_practice/Pointers/pointer_array3.c
+++ C_Practicals/class_practice/Pointers/pointer_array3.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
-void main()
-
+int main()
 {
-
     int arr[5] = {1, 2, 3, 4, 5};
-
     int *p = arr;
-
     int i;
-
     printf("printing array elements...\n");
-
     for (i = 0; i < 5; i++)
-
     {
-
         printf("%d ", *(p + i));
     }
+
+    return 0;
 }
