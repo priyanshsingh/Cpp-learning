dc63ee9dd4460e566cca0c0cc680c9ab16784d92
priyanshsingh <priyanshsingh1401@gmail.com>
2021-01-09 13:19:25 +0530

    added calloc.c

---
+++ C_Practicals/class_practice/DMA/calloc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr;
+    int n;
+    printf("Enter number of elements = ");
+    scanf("%d", &n);
+
+    ptr = (int *)calloc(n, sizeof(int));
+
+    if (ptr == NULL)
+    {
+        printf("\nNO MEMORY ALLOCATED\n");
+        exit(0);
+    }
+    else
+    {
+        printf("\nMEMORY SUCCESSFULLY ALLOCATED USING CALLOC\n");
+        printf("\nEnter %d elements for the array:\n", n);
+
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", (ptr + i));
+        }
+
+        printf("\nElements in the array are:\n");
+
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d\t", *(ptr + i));
+        }
+    }
+
+    return 0;
+}
