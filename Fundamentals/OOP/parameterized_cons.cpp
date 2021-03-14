6dc5a3220ded955f564984c6188ace3b323a0474
priyanshsingh <priyanshsingh1401@gmail.com>
2021-03-14 18:29:58 +0530

    added const and dest

---
+++ Fundamentals/OOP/parameterized_cons.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cars
+{
+private:
+    string company_name;
+    string model_name;
+    string fuel_type;
+    float mileage;
+    double price;
+
+public:
+    Cars()
+    {
+        company_name = "Mercedes from Default Const";
+    }
+    Cars(string mname, string ftype, float mil, double pri)
+    {
+        model_name = mname;
+        fuel_type = ftype;
+        mileage = mil;
+        price = pri;
+    }
+    void setData(string c_name, string m_name, string f_type, float m, double p)
+    {
+        company_name = c_name;
+        model_name = m_name;
+        fuel_type = f_type;
+        mileage = m;
+        price = p;
+    }
+    void DisplayData()
+    {
+        cout << "Car Properties: \n\n";
+        cout << "Car Name: " << company_name << endl;
+        cout << "Model Name: " << model_name << endl;
+        cout << "Fuel Type: " << fuel_type << endl;
+        cout << "Mileage: " << mileage << endl;
+        cout << "Price: " << price << endl;
+    }
+};
+
+int main()
+{
+    Cars car1, car2("Fortuner", "Disel", 10.0, 350000);
+    car1.setData("Toyota", "Altis", "Petrol", 15.5, 150000);
+    car1.DisplayData();
+
+    cout << endl
+         << endl;
+
+    car2.DisplayData();
+
+    return 0;
+}
