3ae9b8085b8795e0bf84a2205a85f8ecbbd43a1d
priyanshsingh <priyanshsingh1401@gmail.com>
2021-09-02 13:11:03 +0530

    added insertion deletion cpp file

---
+++ DSAlgo/searching_algo/insert_del.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+
+void insertion(int LA[],int n,int k,int value)
+{
+    if(k <=n )
+    {
+        int i = 0,j = n;
+        n = n + 1;
+        while(j >= k)
+        {
+            LA[j+1] = LA[j];
+            j = j-1;
+        }
+        LA[k] = value;
+        cout << "New array is : \n";
+        for(i = 0; i < n; i++)
+        {
+            cout << LA[i] << "\n";
+        }
+    }
+    else{
+        cout << "Enter correct index value !!!";
+    }
+}
+
+void deletion(int LA[],int n,int k)
+{
+    int i = 0,j = n;
+    j = k;
+    while(j < n)
+    {
+        LA[j-1] = LA[j];
+       j = j + 1; 
+    }
+    
+    n = n - 1;
+    for(i = 0; i < n; i++)
+    {
+        cout << LA[i] <<"\n";
+    }
+}
+
+int main()
+{   int n;
+    cout << "Enter number of elements to be in array = ";
+    cin >> n;
+    int LA[n];
+    cout << "Enter " << n << " elements in array:\n";
+    for(int i=0;i<n;i++)
+    {
+        cout << "Element number " << i+1 << " = ";
+        cin >> LA[i];
+    }
+
+    int k, value, choice;
+
+    cout << "Which operation you want to perform?\n1. Insertion\n2. Deletion\n(type 1 or 2 for choice.)\n";
+    cin >> choice;
+    switch (choice)
+    {
+        case 1: {
+            cout << "Enter index where new element is to be inserted = ";
+            cin >> k;
+            cout << "Enter item value = ";
+            cin >> value;
+            insertion(LA, n, k, value);
+
+        }
+            break;
+
+        case 2: {
+            cout << "Enter index where new element is to be inserted = ";
+            cin >> k;
+            deletion(LA,n,k);
+
+        }
+            break;
+
+        default: {
+            cout << "WRONG CHOICE ENTERED!";
+        }
+            break;
+
+    }
+   
+
+}
--- DSAlgo/searching_algo/insertion.cpp
+++ DSAlgo/searching_algo/insertion.cpp
@@ -1,32 +1,48 @@
 #include<iostream>
 using namespace std;
-void insertion(int LA[],int n,int k,int item)
+
+void insertion(int LA[],int n,int k,int value)
 {
-    int i=0,j=n;
-    n=n+1;
-    while(j>=k)
+    if(k <=n )
     {
-        LA[j+1]=LA[j];
-        j=j-1;
+        int i = 0,j = n;
+        n = n + 1;
+        while(j >= k)
+        {
+            LA[j+1] = LA[j];
+            j = j-1;
+        }
+        LA[k] = value;
+        cout << "New array is : \n";
+        for(i = 0; i < n; i++)
+        {
+            cout << LA[i] << " ";
+        }
     }
-    LA[k]=item;
-    for(i=0;i<n;i++)
-    {
-        cout<<LA[i]<<" ";
+    else{
+        cout << "Enter correct index value !!!";
     }
 }
+
 int main()
 {   int n;
-    cin>>n;
+    cout << "Enter number of elements to be in array = ";
+    cin >> n;
     int LA[n];
+    cout << "Enter " << n << " elements in array:\n";
     for(int i=0;i<n;i++)
     {
-        cin>>LA[i];
+        cout << "Element number " << i+1 << " = ";
+        cin >> LA[i];
     }
 
     int k;
-    cin>>k;
-    int item;
-    cin>>item;
-    insertion(LA,n,k,item);
+    cout << "Enter index where new element is to be inserted = ";
+    cin >> k;
+    
+    int value;
+    cout << "Enter item value = ";
+    cin >> value;
+
+    insertion(LA, n, k, value);
 }
