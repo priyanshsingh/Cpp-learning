615955d29f18bd5974d410a71d08d57d870f8b7d
priyanshsingh <priyanshsingh1401@gmail.com>
2021-09-14 16:54:37 +0530

    modified singly linked list

--- DSAlgo/linkedLists/singly_ll.cpp
+++ DSAlgo/linkedLists/singly_ll.cpp
@@ -39,6 +39,54 @@ class SinglyLinkedList{
         }
         SinglyLinkedList(Node *n)
         {
+             head = n; 
+        }
+
+//Check if node exists using key value
+        Node* nodeExists(int k)
+        {
+              Node* temp = NULL;
+
+              Node* ptr = head;
+              while(ptr!=NULL)
+              { 
+                  if(ptr->key==k)
+                  {
+                      temp = ptr; 
+                  }
+                  ptr = ptr->next;
+              }
+              return temp;
+        }
+//2. Append Node - attach new node at last of the list
+        void appendNode(Node *n)
+        {
+            if(nodeExists(n->key)!=NULL)
+            {
+                cout << "Node Already exists with key value : " << n->key<<". Append node with any other key value."<<endl;
+            }
+            else
+            {
+                if(head == NULL)
+                {
+                    head = n;
+                    cout << "Node Appended!!!" << endl;
+                }    
+                else
+                {
+                    Node* ptr = head;
+                    while(ptr->key!=NULL)
+                    { 
+                        ptr = ptr->next;
+                    }
+                    ptr->next  = n;
+                    cout << "Node Appended!!!" << endl;
+                }
+            }
+        }
+//3. Prepend Node - attach new node at starting of the list
+        void prependNode()
+        {
 
         }
 };
