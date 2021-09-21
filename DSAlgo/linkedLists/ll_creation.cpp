a6b17f515b6c949182de4e1f6ffa7d751f1558ff
priyanshsingh <priyanshsingh1401@gmail.com>
2021-09-21 13:53:10 +0530

    added ll creation and traversal

--- DSAlgo/linkedLists/ll_creation.cpp
+++ DSAlgo/linkedLists/ll_creation.cpp
@@ -20,19 +20,27 @@ int main()
     struct Node * head;
     struct Node * second;
     struct Node * third;
+    struct Node * fourth;
+
 
     head = (struct Node *)malloc(sizeof(struct Node));
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
 
     head->data = 7;
     head->next = second;
+
     second->data = 8;
     second->next = third;
+    
     third->data = 9;
-    third->next = NULL;
+    third->next = fourth;
 
-    linked_List_Traversal(head);
+    fourth->data = 10;
+    fourth->next = NULL;
+
+    linked_List_Traversal(head);    
 
     return 0;
 }
