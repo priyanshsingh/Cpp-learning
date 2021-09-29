672f98b356d4a631dd3a99a706bf7943ff2cc8eb
priyanshsingh <priyanshsingh1401@gmail.com>
2021-09-29 16:30:31 +0530

    added a test code

---
+++ DSAlgo/practical_mst/2arrayadd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[4];
+    int b[4];
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> b[i];
+    }
+    cout << "\n7\n";
+       
+
+    return 0;
+    
+}
