09d6e6334e6baede8d2c2424a49bfd8d9964e957
priyanshsingh <priyanshsingh1401@gmail.com>
2021-11-06 13:20:00 +0530

    added vector insert function

---
+++ DSAlgo/STL/vector1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void Display(vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << "    ";
+    }
+    cout << endl;
+}
+int main(){
+    vector<int> vec1;
+    int element, size;
+
+    cout << "Enter the size of vector = ";
+    cin >> size;
+    
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter an element to add in this vector = ";
+        cin >> element;
+        vec1.push_back(element);
+    }
+    Display(vec1); 
+    vec1.pop_back();
+    Display(vec1); 
+
+    return 0;
+}
