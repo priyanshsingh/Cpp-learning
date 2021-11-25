d41794ed3f53874436687586202ec007b34d9891
priyanshsingh <priyanshsingh1401@gmail.com>
2021-11-26 00:16:15 +0530

    added stack code

--- DSAlgo/Data_St/stack/pr1.cpp
+++ DSAlgo/Data_St/stack/pr1.cpp
@@ -63,31 +63,49 @@ class Stack{
             return (top+1);
         }
 
-        int peek()
+        int peek(int position)
         {
-            
+            if (isEmpty())
+            {
+            cout << "Stack Underflow!!!"<<endl;
+            return 0;
+            }
+            else
+            return arr[position];
         }
 
         void display()
+    {
+        if (isEmpty())
         {
-            cout << "Display Operation Called!!!";
+            cout << "UNDERFLOW!!!" <<endl;
         }
+        else
+        {
+            cout << "Elements in the Stack: " <<endl;
+            for (int i = 4; i >= 0; i--)
+            {
+                cout << arr[i] << endl;
+            }
+            
+        }        
+    }
 };
 
 
 int main(){
     Stack s1;
-    int choice = 0, item = 0;
+    int choice = 0, item = 0, index = 0, temp1 = 0, countVal = 0, peek1 = 0 ;
 
     do
     {
-        cout << "Enter a number to select the choice of the function that you want to perform. Enter 0 to exit.\n";
+        cout << "\n\n a number to select the choice of the function that you want to perform. Enter 0 to exit.\n";
         cout << "1. Push" << endl;
         cout << "2. Pop" << endl;
         cout << "3. Count" << endl;
         cout << "4. Peek" << endl;
         cout << "5. Display" << endl;
-        cout << "6  . Clear Screen" << endl << endl;
+        cout << "6. Clear Screen" << endl << endl;
         cin >> choice;
         switch (choice)
         {
@@ -100,14 +118,29 @@ int main(){
             break;
         case 2:
             cout << "Pop function called!!!\n";
-            int temp1 = s1.pop();
+            temp1 = s1.pop();
             break;
         case 3:
-            int countVal = s1.count();
+            countVal = s1.count();
+            cout << "The number of Elements in the stack are = " << countVal << endl;
             break;
         case 4:
+            cout << "Peek function called!!!\nEnter the index value = ";
+            cin >> index;
+            if (index<=4)
+            {
+                peek1 = s1.peek(index);
+                cout << "The element at the index, " << index << " is = " << peek1 << endl;
+            }
+            else
+            {
+                cout << "Enter Correct index value!!!";
+            }
             break;
         case 5:
+            s1.display();  
+            break;
+        case 6:
             system("cls");
             break;
 
