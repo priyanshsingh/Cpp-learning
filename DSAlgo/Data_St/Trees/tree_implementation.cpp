eee23583c2732fe3ddb6f502309a583a00cdfc1a
priyanshsingh <priyanshsingh1401@gmail.com>
2022-01-06 20:15:10 +0530

    added simple tree implementation

--- DSAlgo/Data_St/Trees/tree_implementation.cpp
+++ DSAlgo/Data_St/Trees/tree_implementation.cpp
@@ -3,14 +3,24 @@ using namespace std;
 
 struct Node
 {
-    int data,
-    struct Node *left, *right;
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
 };
 
-
 int main(){
-    
+    struct Node * root = new Node(5);
+    root->left = new Node(10);    
+    root->right = new Node(15);    
 
+    root->left->left = new Node(7);    
 
 
     return 0;
