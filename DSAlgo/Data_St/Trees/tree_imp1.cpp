bf8f132c800e56a95d3ba4a8858d3447f9de3a1a
priyanshsingh <priyanshsingh1401@gmail.com>
2022-01-07 12:47:35 +0530

    practice 2 of tree implementation basic

---
+++ DSAlgo/Data_St/Trees/tree_imp1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main(){
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(4);
+
+    root->left->left = new Node(5);
+
+    
+
+    return 0;
+}
