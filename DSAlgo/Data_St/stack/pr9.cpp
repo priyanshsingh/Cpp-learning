63e53b2143c59a7229f941df81feb109b6120d44
priyanshsingh <priyanshsingh1401@gmail.com>
2022-01-19 14:06:48 +0530

    modified same code

--- DSAlgo/Data_St/stack/pr9.cpp
+++ DSAlgo/Data_St/stack/pr9.cpp
@@ -20,12 +20,108 @@ class Stack
         {
             if(top==9)
                 return true;
+            else
+                return false;
+        }
+
+        bool isEmpty()
+        {
+            if(top==-1)
+                return true;
+            else
+                return false;
+        }
+        
+        void push (int data)
+        {
+            if(isFull())
+                cout << "\n\nOVERFLOW!!!\n\n";
+            else
+            {
+                cout << "\nPush function called!\n";
+                top++;
+                arr[top] = data;
+            }
+        }
+
+        void pop()
+        {
+            if(isEmpty())
+                cout << "\n\nUNDERFLOW!!!\n\n";
+            else
+            {
+                cout << "\nPop function called!\n";
+                int temp = arr[top];
+                arr[top] = 0;
+                top--;
+                cout << "\nPopped Element is, " << temp << endl;
+            }
+        }
+
+        void peek()
+        {
+            cout << "\nPeek Function called successfully!\n";
+            cout << "Peek Element is, " << arr[top] << endl;
+        }
+
+        void display()
+        {
+            cout << "Display function called successfully!\n";
+            for (int i = 9; i >= 0; i--)
+            {
+                cout << arr[i] << endl;
+            }
         }
 };
 
 int main()
 {
+    Stack s1;
+    int item, option;
 
+    do
+    {
+        cout << "\n\n**************************STACK DATA STRUCTURE**************************\n\n";
+        cout << "Enter a choice from below given options!\n";
+        cout << "1. Push()" << endl;
+        cout << "2. Pop()" << endl;
+        cout << "3. Peek()" << endl;
+        cout << "4. Display()" << endl;
+        cout << "5. ClearScreen()" << endl;
+        cin >> option;
+
+        switch (option)
+        {
+        case 0:
+            break;
+        
+        case 1:
+            cout << "Enter the value to be pushed into the Stack = ";
+            cin >> item;
+            s1.push(item);
+            break;
+        
+        case 2:
+            s1.pop();
+            break;
+        
+        case 3:
+            s1.peek();
+            break;
+        
+        case 4:
+            s1.display();
+            break;
+        
+        case 5:
+            system("cls");
+            break;
+        
+        default:
+            break;
+        }
+    } while (option!=0);
+    
 
     return 0;
 }
