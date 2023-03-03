1634aeecafae65b5daa1b56234e0ef90d7191368
priyanshsingh <priyanshsingh1401@gmail.com>
2023-03-03 13:18:46 +0530

    added stack folder in th e repo

---
+++ DSAlgo/Data_St/heap_aditya_verma/heap_and_identification.cpp
@@ -0,0 +1,24 @@
+// Heap can be represented using stack.
+// It is of a fixed size
+
+// Identification
+// 1. Find the keywrd "k" in the question
+// 2. To find smallest or largest
+
+// Heap can be applied in such a question which can satisfy both these conditions
+
+// Which heap to choose:
+// 1. Min Heap
+// 2. Max Heap
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+
+
+    return 0;
+}
